e57659e85319a11134fc2524073aeefa6f964801
79521 <79521@MSI>
2024-04-13 19:50:34 +0700

    Добавьте файлы проекта.

---
+++ лаба 11 задание 2/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace лаба_11_задание_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.sorting = new System.Windows.Forms.Panel();
+            this.namess = new System.Windows.Forms.Button();
+            this.price = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Searh = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.sorting.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 446);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Генерация";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(0, 15);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(269, 410);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 24;
+            this.listBox2.Location = new System.Drawing.Point(458, 15);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(276, 410);
+            this.listBox2.TabIndex = 4;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(616, 446);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(89, 29);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // sorting
+            // 
+            this.sorting.Controls.Add(this.namess);
+            this.sorting.Controls.Add(this.price);
+            this.sorting.Controls.Add(this.button2);
+            this.sorting.Location = new System.Drawing.Point(303, 307);
+            this.sorting.MaximumSize = new System.Drawing.Size(121, 91);
+            this.sorting.MinimumSize = new System.Drawing.Size(121, 31);
+            this.sorting.Name = "sorting";
+            this.sorting.Size = new System.Drawing.Size(121, 31);
+            this.sorting.TabIndex = 7;
+            // 
+            // namess
+            // 
+            this.namess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.namess.Location = new System.Drawing.Point(3, 66);
+            this.namess.Name = "namess";
+            this.namess.Size = new System.Drawing.Size(115, 25);
+            this.namess.TabIndex = 2;
+            this.namess.Text = "Имя товара";
+            this.namess.UseVisualStyleBackColor = true;
+            this.namess.Click += new System.EventHandler(this.namess_Click);
+            // 
+            // price
+            // 
+            this.price.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.price.Location = new System.Drawing.Point(3, 40);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(115, 26);
+            this.price.TabIndex = 1;
+            this.price.Text = "Цена";
+            this.price.UseVisualStyleBackColor = true;
+            this.price.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(0, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 28);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Сортировка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Searh
+            // 
+            this.Searh.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Searh.Location = new System.Drawing.Point(518, 443);
+            this.Searh.Name = "Searh";
+            this.Searh.Size = new System.Drawing.Size(92, 33);
+            this.Searh.TabIndex = 8;
+            this.Searh.Text = "Поиск";
+            this.Searh.UseVisualStyleBackColor = true;
+            this.Searh.Click += new System.EventHandler(this.Searh_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Clear.Location = new System.Drawing.Point(140, 446);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(174, 32);
+            this.Clear.TabIndex = 9;
+            this.Clear.Text = "Очистить поиск";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Save
+            // 
+            this.Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Save.Location = new System.Drawing.Point(320, 446);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(118, 32);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Сохранить";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(726, 490);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Searh);
+            this.Controls.Add(this.sorting);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.sorting.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel sorting;
+        private System.Windows.Forms.Button namess;
+        private System.Windows.Forms.Button price;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Searh;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Save;
+    }
+}
+
---
+++ лаба 11 задание 2/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace лаба_11_задание_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+        bool iscollapsed = true;
+        string god;
+        struct Goods
+        {
+            public string name;
+            public string nameOfShop;
+            public int price;
+
+            public override string ToString()
+            {
+                return $"{name} - {nameOfShop} - {price}";
+            }
+        }
+        string[] names = new string[13] { "Milk", "Eggs", "Bananas", "Chicken", "Oatmeal", "Pasta", "Backwheat", "Liquid", "Water", "Curd", "Apple", "Grape", "Bread" };
+        string[] nameOfShops = new string[5] { "Lenta", "Metro", "Yarche!", "Pyatorochka", "Magnit" }; // нет смысла здесь держать списки, если они все равно не будут расширяться 
+        // price будет генерироваться с помощью метода random;
+        List<Goods> goods = new List<Goods>();
+        string path = @"C:\\Users\\79521\\OneDrive\\Рабочий стол\\Для учебы\\ВКП\\лабораторная работа ОП 1\\11 лаба\resource.txt";
+        private void Generation()
+        {
+            for (int i = 0; i < 10; i++)// можно отдельным перменным присвоить рандомные значения каждого из массивов, которые содержат информацию о наименовании продукта, имени магазина и стоимости товара, а потом 
+                                        // в список с типом данных Goods, в котором 3 переменные, присвоить их объекту структуры, т.е создать ее, а потом добавить в заранее созданный список, содержащий объекты данной структуры
+            {
+                string nameOfGoods = names[random.Next(names.Length)];
+                string nameOfshop = nameOfShops[random.Next(nameOfShops.Length)];
+                int price = random.Next(50, 1000);
+                // все переменные выше образуют одну позицию товара, включая имя товара, название магазина, в котором он есть, и цену за этот товар в этом магазине 
+                Goods posistion = new Goods { name = nameOfGoods, nameOfShop = nameOfshop, price = price }; // Создан объект структуры Goods, в котором находится одна позиция. Теперь этот объект можно добавить в список, который как раз принимает эти объекты
+                // для этого обязательно было создавать новый объект структуры
+                goods.Add(posistion);
+            }
+            
+            using (var write = new StreamWriter(path, false)) // false - потому что мне не нужно каждый раз добавлять в файл новые данные, которые по сути могут совпадать
+            {
+                for (int i = 0; i < goods.Count; i++)
+                {
+                    write.WriteLine(goods[i]);
+                }
+            }
+            using (var sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    listBox1.Items.Add(sr.ReadLine()); // позовляет добавить элемент с листбокс
+                }
+            }
+            
+        }
+
+        private void SortInNames(List<Goods> goods)
+        {
+            listBox2.Items.Clear();
+            IEnumerable<Goods> sortlist =
+            from names in goods
+            orderby names.name //"ascending" is default
+            select names;
+            foreach (Goods name in sortlist)
+            {
+                listBox2.Items.Add(name);
+            }
+        }
+        private void SortInPrice(List<Goods> goods) // данный метод сортировки спокойно подходит и для числовых значений
+        {
+            listBox2.Items.Clear();
+            IEnumerable <Goods> sortlist =
+                from price in goods
+                orderby price.price 
+                select price;
+            foreach (Goods name in sortlist)
+            {
+                listBox2.Items.Add(name);
+            }
+        }
+        private bool Search(string god, List<Goods> goods)
+        {
+            int count = 0;
+            listBox2.Items.Clear();
+            foreach (Goods name in goods) 
+            {
+                if (name.name == god) 
+                {
+                    listBox2.Items.Add(name);
+                    count++;
+                }
+            }
+            if (count == 0) 
+            {
+                listBox2.Items.Clear();
+                MessageBox.Show("Такого нет в списках");
+                return false;
+            }
+            return true;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            listBox1.Items.Clear();
+            Generation();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (iscollapsed)
+            {
+                sorting.Height += 10;
+                if (sorting.Size == sorting.MaximumSize)
+                {
+                    timer1.Stop();
+                    iscollapsed = false;
+                }
+            }
+            else
+            {
+                sorting.Height -= 10;
+                if (sorting.Size == sorting.MinimumSize)
+                {
+                    timer1.Stop();
+                    iscollapsed = true;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e) // price
+        {
+            SortInPrice(goods);
+        }
+
+        private void namess_Click(object sender, EventArgs e)
+        {
+            SortInNames(goods);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Searh_Click(object sender, EventArgs e)
+        {
+            god = textBox1.Text;
+            bool finder = Search(god, goods);
+            if (!finder)
+            {
+                Thread.Sleep(2000);
+                listBox2.Items.Clear();
+                textBox1.Text = null;
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            listBox2.Items.Clear();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            string generateName = "abcdefghijklmnopqrstuvwxyz";
+            string nameOfFile = null;
+            if (listBox2 != null) 
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    nameOfFile += Convert.ToString(generateName[random.Next(generateName.Length)]);
+                }
+                using(var file = new StreamWriter($@"C:\Users\79521\OneDrive\Рабочий стол\Для учебы\ВКП\лабораторная работа ОП 1\11 лаба\{nameOfFile}.txt"))
+                {
+                    foreach (Goods item in listBox2.Items)
+                    {
+                        file.WriteLine(item);
+                    }
+                }
+                
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
